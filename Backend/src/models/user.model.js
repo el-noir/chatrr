282dbf083c0ad1f00163e27f748172f3421affe6
el-noir <mudasirshah9777@gmail.com>
2025-03-01 19:01:09 +0500

    User Model

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+        email: {
+            type: String,
+            required: [true, "Email is required."],
+            unique: true,
+        }, 
+        password: {
+            type: String,
+            required: [true, "Password is required."],
+        }, 
+        firstName: {
+            type: String, 
+            required: false,
+        },
+        lastName: {
+            type: String, 
+            required: false,
+        },
+            image: {
+            type: String, 
+            required: false,
+        },
+        color: {
+            type: Number,
+            required: false
+        }, 
+        profileSetup: {
+            type: Boolean,
+            default: false,
+        }
+        
+})
+
+export const User = mongoose.model('User', userSchema);
