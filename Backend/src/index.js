fddcc6437e39f97c5f089d3065ceecc08919c18e
el-noir <mudasirshah9777@gmail.com>
2025-03-01 18:12:12 +0500

    dotenv configured

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv'
 
 const app= express();
+
+dotenv.config({
+    path: './.env'
+})
+
 const port = process.env.PORT || 8000
+
 
 app.listen(port, ()=>{
     console.log(`Server is listening on http://localhost:${port}`)
