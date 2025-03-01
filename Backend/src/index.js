db531f4c59d81690c64a46ec818fd26ab95c20a9
el-noir <mudasirshah9777@gmail.com>
2025-03-01 18:09:40 +0500

    basic server created

---
+++ Backend/src/index.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app= express();
+const port = process.env.PORT || 8000
+
+app.listen(port, ()=>{
+    console.log(`Server is listening on http://localhost:${port}`)
+})
+
