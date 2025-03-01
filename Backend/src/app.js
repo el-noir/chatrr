18831af167ae7470cd0b2b3796cb4414d6bdc0a2
el-noir <mudasirshah9777@gmail.com>
2025-03-01 19:54:12 +0500

    auth route and some debugging

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 import express, {urlencoded} from "express"
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
+import authRoutes from "./routes/auth.route.js";
 const app = express();
 
 app.use(cors({
@@ -22,5 +23,7 @@ app.use(express.static("public"));
 app.use(cookieParser(
     
 ));
+
+app.use('/api/v1/auth',authRoutes);
 
 export {app}
--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,12 +1,12 @@
-import { User } from "../models/user.model";
-import {sign} from "jsonwebtoken"
+import { User } from "../models/user.model.js";
+import jwt from "jsonwebtoken"
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 
 const createToken = (email, userId) =>{
-    return sign({email, userId}, process.env.JWT_KEY, {expiresIn: maxAge})
+    return jwt.sign({email, userId}, process.env.JWT_KEY, {expiresIn: maxAge})
 }
 
-export const signup = async(request, response, next)=>{
+const signup = async(request, response, next)=>{
    try{
       const {email, password} = request.body;
       if(!email || !password){
@@ -33,3 +33,5 @@ export const signup = async(request, response, next)=>{
     return response.status(500).send("Internal Server Error");
    }
 };
+
+export {signup}
