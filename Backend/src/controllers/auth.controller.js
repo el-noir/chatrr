390ceb13292be5823e499099506c0e3fd4f03141
el-noir <mudasirshah9777@gmail.com>
2025-03-04 02:17:06 +0500

    login controller

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -34,4 +34,38 @@ const signup = asyncHandler(async (req, res, next) => {
     }
 });
 
+const login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+      return next(new ApiErr(400, "Email and Password are required."));
+  }
+
+  try {
+      const user = await User.findOne({ email});
+      if(!user){
+        return new ApiErr(404, "User with the given email not found.")
+      }
+
+      const auth = await compare(password, user.password);
+
+      if(!auth){
+        return ApiErr(400, "Password is incorrect.");
+      }
+
+      res.cookie("jwt", createToken(email, user.id), {
+          maxAge,
+          secure: true,
+          sameSite: "None",
+      });
+
+      return res
+          .status(200)
+          .json(new ApiRes(200, { id: user.id, email: user.email, profileSetup: user.profileSetup, firstName: user.firstName, lastName: user.lastName, image: user.image, color: user.color }, "User created successfully"));
+  } catch (error) {
+      return next(new ApiErr(500, "Internal Server Error", [error.message]));
+  }
+});
+
+
 export { signup };
